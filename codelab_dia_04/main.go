291d00d1d7996765b7270273d24dfaf3f0a2ff89
Vitoria Mendes <vitoriamendes95@gmail.com>
2019-10-05 17:29:38 -0300

    First exercise

--- codelab_dia_04/main.go
+++ codelab_dia_04/main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	exercise1()
+}
+
+func exercise1() {
+	intArray := [10]int{}
+	intArray[0] = 2
+	intArray[1] = 4
+	intArray[2] = 5
+	intArray[3] = 6
+	intArray[4] = 10
+	intArray[5] = 12
+	intArray[6] = 14
+	intArray[7] = 15
+	intArray[8] = 20
+	intArray[9] = 24
+
+	fmt.Print(intArray)
+	fmt.Println("")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(intArray[i])
+	}
+
 }
