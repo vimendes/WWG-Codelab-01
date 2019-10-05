c2d14b172f4a82b66f86f0ba1815b6597b499178
Vitoria Mendes <vitoriamendes95@gmail.com>
2019-10-05 16:20:18 -0300

    Coded already made at 2nd codelab

--- codelab_dia_02/main.go
+++ codelab_dia_02/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("coloque suas resoluções nesse arquivo :D")
+	nameStrings()
+}
+
+func nameStrings() {
+	nomes := [3]string{"vitoria", "erika", "amanda"}
+	for _, nome := range nomes {
+		saudacao(nome)
+	}
+
+	var ano int
+	ano = 2019
+
+	fmt.Printf("Binário: %b\n  Decimal: %d\n Hexa: %X", ano, ano, ano)
+}
+
+func saudacao(nome string) {
+	fmt.Println("Hello, ", nome)
 }
